888c311fec8147e9ff514b3ded9620d038d9d3b9
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-09-04 00:20:16 +0100

    Descending order (7)

---
+++ 7kyu/descendingOrder.js
@@ -0,0 +1,16 @@
+"https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/javascript";
+
+function descendingOrder(n) {
+    return Number(
+        n
+            .toString()
+            .split("")
+            .map(Number)
+            .sort((a, b) => {
+                return b - a;
+            })
+            .join("")
+    );
+}
+
+// Or .sort() (sorts ascending by default) and . reverse()
