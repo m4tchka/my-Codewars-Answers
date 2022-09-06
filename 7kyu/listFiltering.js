bf55d964d627fc975833d9eaee1fe811ca3c8b51
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-09-06 23:59:40 +0100

    better solution for listFiltering

--- 7kyu/listFiltering.js
+++ 7kyu/listFiltering.js
@@ -1,4 +1,14 @@
-"https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/javascript"
+"https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/javascript";
+
+function filter_list(l) {
+    let l2 = l.filter(checkType);
+    return l2;
+}
+
+function checkType(e) {
+    return typeof e === "number";
+}
+// ALTERNTATIVELY ---------
 
 function filter_list(l) {
     let arr = [];
