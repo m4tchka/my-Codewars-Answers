b5e147e4e52affb7d9b819b4b1397f5dd1d9aaf6
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-09-18 18:21:24 +0100

    twiceLinear(4) + comments

--- 4kyu/twiceLinear.js
+++ 4kyu/twiceLinear.js
@@ -1,48 +1,39 @@
 "https://www.codewars.com/kata/5672682212c8ecf83e000050/train/javascript";
 
 function dblLinear(n) {
-    let u = [1];
-    let yIndex = 0;
+    let u = [1]; 
+    // Define starting array
+    let yIndex = 0; 
     let zIndex = 0;
-
+   // Define 2 variables corresponding to the indexes of u, where the entry in u at that index will be compared against the entry at the other index
     while (u.length <= n) {
+        // The array will be pushed to each time, so stop when it is length n
         let y = 2 * u[yIndex] + 1;
         let z = 3 * u[zIndex] + 1;
+        // Define 2 variables - the possible values to be pushed to the array on each loop
         if (y < z) {
             yIndex++;
         }
         if (z < y) {
             zIndex++;
         }
+        /*
+        The smaller of the 2 variables will be pushed, so increment that variable's index (so that on the next loop, that variable will be a different number,
+        calculated from the next entry in the array.
+        */
         if (z == y) {
             yIndex++;
             zIndex++;
         }
+        // If the variables happen to be the same, increment both indexes (so that a specific value doesn't get pushed to u again on the next loop (i.e. no duplicates))
         u.push(Math.min(y, z));
+        //Push the smaller variable
     }
     return u[n];
+    // return last entry (which also happens to be entry n)
 }
 
 /* 
-function dblLinear(n) {
-    let u = [1];
-    u = addItem(u, n);
-    //u=u.sort((a,b)=>{return a-b})
-
-    console.log(u);
-    function addItem(arr, n) {
-        console.log("hi");
-        let y = 2 * arr[arr.length - 1] + 1;
-        let z = 3 * arr[arr.length - 1] + 1;
-        if (!u.includes(y)) {
-            u.push(y);
-        }
-        if (!u.includes(z)) {
-            u.push(z);
-        }
-        if (arr.length == n) {
-            return addItem(arr);
-        }
-    }
-}
+    REVIEW:
+    TLDR: 2 indexes to track variables, push only 1 (the smaller) at a time per loop and increment its index
 */
