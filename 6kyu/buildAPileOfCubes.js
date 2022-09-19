c59f115cf7d29725556dae6f5d2e470367411216
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-09-19 23:45:37 +0100

    buildAPileOfCubes(6) - better implementation

--- 6kyu/buildAPileOfCubes.js
+++ 6kyu/buildAPileOfCubes.js
@@ -1,6 +1,28 @@
 "https://www.codewars.com/kata/5592e3bd57b64d00f3000047/train/javascript";
 
 /* 
+    Second attempt - faster - calls countUp with number to be checked, current bottom-most cube (the one with longest sides) and current sum of volume of all cubes.
+    Passing sum down means not having to recalculate it each time - simply: 
+        Add the new cube's value to the volume sum
+        Check if that volume sum is greater or equal to the target value, m (returning -1 (overshoot, no valid solution), or the largest cube's side length)
+        If it is smaller, then recursively call again but with n + 1 
+*/
+
+function findNb(m) {
+    return countUp(m, 1, 0);
+}
+function countUp(m, n, sum) {
+    sum += n ** 3;
+    if (sum > m) {
+        return -1;
+    } else if (sum == m) {
+        return n;
+    } else {
+        return countUp(m, n + 1, sum);
+    }
+}
+
+/* 
     First attempt - very slow - recalculates sum on each function call --- (BUT IT WORKED FIRST TIME !!)
 */
 
