5c172ab5f90c786e8019a21d88d82c4177921ea2
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-09-24 21:52:02 +0100

    productOfConsecutiveFibNumbers(5) explanation

--- 5kyu/productOfConsecutiveFibNumbers.js
+++ 5kyu/productOfConsecutiveFibNumbers.js
@@ -13,3 +13,11 @@ function fib(fn, fn1, prod) {
         return fib(fn1, fn1 + fn, prod);
     }
 }
+
+/* 
+    Recursive solution, calls a function, with 2 consecutive numbers of fib sequence and the target product
+    FAILURE CONDITION: If the product of the 2 fib nubmers is greater than the target product (i.e. overshoot) return the numbers and false in an arr (breaks the "loop")
+    SUCCESS CONDITION: If the product of the 2 fib nubmers equals the target product, return numbers and true in an arr (also breaks the "loop")
+    CONTINUE CONDITION: If the product of the 2 fib numbers is less than the target product (i.e. undershoot) call the function again, but with the next fib nubmers
+        NOTE: DON'T JUMP OVER 2 POSITIONS IN SEQUENCE (Ex. 1st & 2nd -> 3rd & 4th), instead like a "slinky" (i.e. 1st & 2nd -> 2nd & 3rd -> 3rd & 4th)
+*/
