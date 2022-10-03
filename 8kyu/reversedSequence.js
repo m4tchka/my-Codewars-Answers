26d4fcdce52ded634cc2db7d60a04d6497a13404
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-10-03 23:58:40 +0100

    reversedSequence(8)

---
+++ 8kyu/reversedSequence.js
@@ -0,0 +1,9 @@
+"https://www.codewars.com/kata/5a00e05cc374cb34d100000d/train/javascript";
+
+const reverseSeq = (n) => {
+    let z = [];
+    for (let i = n; i > 0; i--) {
+        z.push(i);
+    }
+    return z;
+};
