921fbe245aa1e10bbda7f58230439479ae3f1123
m4tchka <91279022+m4tchka@users.noreply.github.com>
2023-04-25 21:37:19 +0100

    categorizeNewMember alt

--- 7kyu/categorizeNewMember.js
+++ 7kyu/categorizeNewMember.js
@@ -1,11 +1,17 @@
-"https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa/train/javascript"
+"https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa/train/javascript";
 
-function openOrSenior(data){
-    let members = []
-    for (let i = 0 ; i < data.length; i ++ ) {
-      if (data[i][0]>= 55 && data[i][1] > 7) {
-        members.push("Senior")
-      } else members.push("Open")
+function openOrSenior(data) {
+    let members = [];
+    for (let i = 0; i < data.length; i++) {
+        if (data[i][0] >= 55 && data[i][1] > 7) {
+            members.push("Senior");
+        } else members.push("Open");
     }
-    return members
-  }
+    return members;
+}
+
+function openOrSenior(data) {
+    return data.map((e) => {
+        return e[0] >= 55 && e[1] > 7 ? "Senior" : "Open";
+    });
+}
