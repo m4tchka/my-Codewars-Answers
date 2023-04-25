f66e76e6c4ee0f92ff543438362eb4f54c8581c2
m4tchka <91279022+m4tchka@users.noreply.github.com>
2023-04-25 20:55:48 +0100

    categorizeNewMember 7

---
+++ 7kyu/categorizeNewMember.js
@@ -0,0 +1,11 @@
+"https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa/train/javascript"
+
+function openOrSenior(data){
+    let members = []
+    for (let i = 0 ; i < data.length; i ++ ) {
+      if (data[i][0]>= 55 && data[i][1] > 7) {
+        members.push("Senior")
+      } else members.push("Open")
+    }
+    return members
+  }
